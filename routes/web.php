a3248b19469252b0479788a3ac03c8cf474aa486
samuderapang <samuderapanggabean@gmail.com>
2025-07-01 19:50:17 +0700

    Add Render deployment configuration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReportController;
-
-Route::get('/report/transactions/{year}/{month}', [ReportController::class, 'downloadTransactionsPdf'])
-    ->name('report.transactions.pdf');
+use App\Http\Controllers\ReportController; // Diperlukan untuk rute report PDF
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +14,14 @@ Route::get('/report/transactions/{year}/{month}', [ReportController::class, 'dow
 |
 */
 
+// Rute 1: Mengalihkan halaman utama (root) ke panel admin
 Route::redirect('/', '/admin');
+
+// Rute 2: Menangani permintaan download laporan PDF
+Route::get('/report/transactions/{year}/{month}', [ReportController::class, 'downloadTransactionsPdf'])
+    ->name('report.transactions.pdf');
+
+// Rute 3: Diperlukan oleh Render untuk memeriksa apakah aplikasi berjalan (Health Check)
+Route::get('/up', function() {
+    return response('OK', 200);
+});
