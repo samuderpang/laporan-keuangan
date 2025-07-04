27cd8264a1453d159d77dc5bac12c8f1e5eba159
samuderapang <samuderapanggabean@gmail.com>
2025-07-04 23:07:40 +0700

    Make transaction image nullable

--- database/migrations/2025_06_29_122803_create_transactions_table.php
+++ database/migrations/2025_06_29_122803_create_transactions_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->date('date');
             $table->integer('amount');
             $table->string('note');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
